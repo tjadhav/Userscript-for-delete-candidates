2f97a4e480a646617fb9a2ae85982a99d5fda139
tusharjadhav219 <tusharjadhav219@gmail.com>
2017-02-14 09:00:44 +0530

    Changed version

--- script_jquery.user.js
+++ script_jquery.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Red-Green-Blue for Delete Candidates
 // @namespace    https://github.com/tusharjadhav219/Userscript-for-delete-candidates
-// @version      0.4
+// @version      0.4.1
 // @description  For https://gist.github.com/sotodel/0a2d92faa6c08192efed94fd4044a9cc.
 // @author       Tushar
 // @match        https://gist.github.com/sotodel/*
